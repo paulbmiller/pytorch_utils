627076f05ee652fb160cae4e8f8e7bf23be73f60
Paul Miller <miller_p@hotmail.fr>
2020-10-07 14:52:49 +0200

    Changed calculation for convolutional shapes

--- conv_calc.py
+++ conv_calc.py
@@ -1,10 +1,17 @@
+import numpy as np
+
 def conv_t_size(inp, k, stride=1, out_pad=0, pad=0, dil=1):
     """Returns the output size of a ConvTranspose operation given the size of
     the square-shaped input `inp` and the kernel size `k`. Optional arguments
     are stride, `out_pad` (padding on one side of the output), `pad` (padding
     on both sides of the input), `dilation` (spacing between kernel elements).
     """
-    return (inp-1)*stride-2*pad+dil*(k-1)+out_pad+1
+    inp = np.asarray(inp)
+    out = (inp-1)*stride-2*pad+dil*(k-1)+out_pad+1
+    if out.size > 1:
+        return tuple(out)
+    else:
+        return out.item()
 
 
 def conv_size(inp, k, stride=1, pad=0, dil=1):
@@ -13,4 +20,9 @@ def conv_size(inp, k, stride=1, pad=0, dil=1):
     stride, `pad` (padding on both sides of the input), `dilation` (spacing
     between kernel elements).
     """
-    return (inp+2.*pad-dil*(k-1)-1)/stride+1
+    inp = np.asarray(inp)
+    out = (inp+2.*pad-dil*(k-1)-1)/stride+1
+    if out.size > 1:
+        return tuple(out)
+    else:
+        return out.item()
